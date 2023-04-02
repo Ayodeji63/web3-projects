77fd8759876885e535f66790a19b615aa02b9f04
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-02 14:16:14 +0100

    Started the designs for the NFT Components

--- Uniswap-clone/nextjs-blog/pages/NFT.jsx
+++ Uniswap-clone/nextjs-blog/pages/NFT.jsx
@@ -5,7 +5,26 @@ const Nft = () => {
   return (
     <div>
         <Header/>
-        Nft
+        <div className='nfts-container'>
+            <div className='nft-cards'>
+                <div className='nft-card'>
+                    <img src={"/first-nft-image.avif"} alt="" />
+                    <span className="owner">NFT1</span>
+                    <h5 className='nft-name'>Reward NFT2 #8245</h5>
+                    <div className='price-container'>
+                        <div className='price'>
+                            <span>Price</span>
+                            <h5>150 MATIC</h5>
+                        </div>
+                        <div className='bid'>
+                            <span>Highest bid</span>
+                            <h5>No bids yet</h5>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
         
     </div>
   )
