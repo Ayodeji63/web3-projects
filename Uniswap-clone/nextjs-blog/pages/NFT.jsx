bbde379caefed2d9b5ac033bb97986e76a01a7f9
Olagboye Seyi <akingbolaola@gmail.com>
2023-04-02 15:58:11 +0100

    Finished the NFT component Design only remain some few changes

--- Uniswap-clone/nextjs-blog/pages/NFT.jsx
+++ Uniswap-clone/nextjs-blog/pages/NFT.jsx
@@ -1,40 +1,38 @@
-import Image from 'next/image'
-import React from 'react'
-import Header from './components/Header'
-import { nftData } from './nftData'
+import Image from "next/image";
+import React from "react";
+import Header from "./components/Header";
+import { nftData } from "./nftData";
 
 const Nft = () => {
   return (
     <div>
-        <Header/>
-        <div className='nfts-container'>
-            <h2 className='nft-text'>NFT Collections</h2>
-            <div className='nft-cards'>
-                {
-                    nftData.map((nft) => (
-                        <div className='nft-card'>
-                    <Image src={nft.nft_Image} alt="" />
-                    <span className="owner">{nft.owner}</span>
-                    <h5 className='nft-name'>{nft.nft_Name}</h5>
-                    <div className='price-container'>
-                        <div className='price'>
-                            <span>Price</span>
-                            <h5>{nft.price}</h5>
-                        </div>
-                        <div className='bid'>
-                            <span>Highest bid</span>
-                            <h5>{nft.bid}</h5>
-                        </div>
-
-                    </div>
+      <Header />
+      <div className="nfts-container">
+        <h2 className="nft-text">NFT Collections</h2>
+        <div className="nft-cards">
+          {nftData.map((nft) => (
+            <a href="#">
+              <div className="nft-card">
+                <Image src={nft.nft_Image} alt="" />
+                <span className="owner">{nft.owner}</span>
+                <h5 className="nft-name">{nft.nft_Name}</h5>
+                <div className="price-container">
+                  <div className="price">
+                    <span>Price</span>
+                    <h5>{nft.price}</h5>
+                  </div>
+                  <div className="bid">
+                    <span>Highest bid</span>
+                    <h5>{nft.bid}</h5>
+                  </div>
                 </div>
-                    ))
-                }
-            </div>
+              </div>
+            </a>
+          ))}
         </div>
-        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nft
+export default Nft;
